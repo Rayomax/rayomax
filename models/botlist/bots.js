5692c619ef042d25de01268d9fa0a4919a48b0b0
Glitch (newdiscordspace) <none>
2021-07-28 22:01:35 +0000

    🏎😇 Updated with Glitch

---
+++ models/botlist/bots.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+let hm = new mongoose.Schema({
+ownerID: String,
+ownerName: String,
+botID: String,
+username: String,
+discrim: String,
+avatar: String,
+prefix: String,
+longDesc: String,
+shortDesc: String,
+tags: Array,
+coowners: Array,
+status: String,
+website: String,
+github: String, 
+support: String,
+privateURL: String,
+Date: {type: Date, default: null},
+certificate: String,
+votes: {type: Number, default: 0}, 
+backURL: String
+});
+
+module.exports = mongoose.model("bots", hm);
